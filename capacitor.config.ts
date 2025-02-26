a740856c0e830816f2609c9b1d2567b1477f4f0f
robertoinfor <robertoinfor03@gmail.com>
2025-02-26 14:05:13 +0000

    inicio de repositorio

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Fichaje_Lugar',
+  webDir: 'dist'
+};
+
+export default config;
