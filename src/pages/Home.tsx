0306622930daa9d242e3e58786af519272abd777
robertoinfor <robertoinfor03@gmail.com>
2025-02-27 09:39:43 +0000

    inicio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LogIn from '../components/LogIn';
+
 
 const Home: React.FC = () => {
   return (
@@ -17,6 +20,8 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
+        <button className="btn btn-primary">Bootstrap Button</button>
+        <LogIn/>
       </IonContent>
     </IonPage>
   );
